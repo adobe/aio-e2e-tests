4eaf44a4d6d5c02a46f9ce056a24364700365d56
Moritz Raho <raho@adobe.com>
2021-03-10 16:44:08 +0100

    revert tmp commits

--- src/auth.js
+++ src/auth.js
@@ -43,8 +43,11 @@ async function getJWTToken (options) {
     privateKey,
     passphrase = '',
     metaScopes = [
-      'https://ims-na1.adobelogin.com/s/ent_user_sdk',
-      'https://ims-na1.adobelogin.com/s/ent_adobeio_sdk'
+      'https://ims-na1.adobelogin.com/s/ent_analytics_bulk_ingest_sdk',
+      'https://ims-na1.adobelogin.com/s/ent_marketing_sdk',
+      'https://ims-na1.adobelogin.com/s/ent_campaign_sdk',
+      'https://ims-na1.adobelogin.com/s/ent_adobeio_sdk',
+      'https://ims-na1.adobelogin.com/s/ent_audiencemanagerplatform_sdk'
     ],
     ims = 'https://ims-na1.adobelogin.com'
   } = options
