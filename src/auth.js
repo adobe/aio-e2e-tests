6f66e21b6fde70c014bc94e432c41934fe82fd9e
Moritz Raho <raho@adobe.com>
2025-07-17 10:08:37 +0200

    fix: update to s2s credentials (#178)
    * fix: update to s2s credentials
    
    * fix env
    
    * events tests still rely on a variable named EVENTS_JWT_TOKEN

--- src/auth.js
+++ src/auth.js
@@ -13,175 +13,59 @@ governing permissions and limitations under the License.
 /* eslint-disable camelcase */
 
 const fetch = require('node-fetch')
-const jwt = require('jsonwebtoken')
 const FormData = require('form-data')
 
-const JWT_EXPIRY_SECONDS = 1200 // 20 minutes
-
 /**
- * Create a jwt payload.
- *
- * @param {object} options see getSignedJwt
- * @param {number} millisecondsSinceEpoch the date in ms since epoch
- * @returns {object} the payload
+ * @param {string} env ims env
+ * @param {string} clientId clientId
+ * @param {string} clientSecret clientSecret
+ * @param {string} orgId imsOrgId
+ * @param {string} scopes coma separated string
+ * @returns {{ access_token: string }} ims response
  */
-function createJwtPayload (options, millisecondsSinceEpoch = Date.now()) {
-  let m = options.metaScopes
-  if (!Array.isArray(m)) {
-    m = m.split(',')
+async function getAccessTokenByClientCredentials (env, clientId, clientSecret, orgId, scopes) {
+  const IMS_ENDPOINTS = {
+    stage: 'https://ims-na1-stg1.adobelogin.com',
+    prod: 'https://ims-na1.adobelogin.com'
+  }
+  const endpoint = IMS_ENDPOINTS[env]
+  if (!endpoint) {
+    throw new Error(`IMS_ENV must be one of "${Object.keys(IMS_ENDPOINTS)}"`)
   }
 
-  const metaScopes = {}
-  m.forEach(m => {
-    if (m.startsWith('https')) {
-      metaScopes[m] = true
-    } else {
-      metaScopes[`${options.ims}/s/${m}`] = true
-    }
-  })
-
-  return {
-    aud: `${options.ims}/c/${options.clientId}`,
-    exp: Math.round(JWT_EXPIRY_SECONDS + millisecondsSinceEpoch / 1000),
-    ...metaScopes,
-    iss: options.orgId,
-    sub: options.technicalAccountId
+  // prepare the data with common data
+  const postData = {
+    grant_type: 'client_credentials',
+    client_id: clientId,
+    client_secret: clientSecret,
+    org_id: orgId,
+    scope: scopes
   }
-}
-
-/**
- * Gets an OAuth token.
- *
- * @param {string} actionURL the url to fetch the token from
- * @returns {object} the token data
- */
-async function getOauthToken (actionURL) {
-  const postOptions = {
-    method: 'POST'
-  }
-
-  const res = await fetch(actionURL, postOptions)
-  const json = await res.json()
-  const { access_token, error, error_description } = json
-  if (!access_token) {
-    if (error && error_description) {
-      throw new Error(`${error}: ${error_description}`)
-    } else {
-      throw new Error(`An unknown error occurred fetching oauth token. The response is as follows: ${JSON.stringify(json)}`)
-    }
-  }
-  return json
-}
-
-/**
- * Gets a signed JWT.
- *
- * @param {object} options all the options for generating the JWT
- * @param {string} options.clientId the jwt client id
- * @param {string} options.technicalAccountId the technical account id of the credential
- * @param {string} options.orgId the org id of the credential
- * @param {string} options.clientSecret the jwt client secret
- * @param {string} options.privateKey the jwt private key
- * @param {string} [options.passphrase=''] the passphrase for private key, if set
- * @param {Array<string>} options.metaScopes all the metascopes for the services tied to the credential
- * @param {string} [options.ims='https://ims-na1.adobelogin.com'] the IMS endpoint
- * @returns {string} the signed jwt
- */
-async function getSignedJwt (options) {
-  const {
-    clientId,
-    technicalAccountId,
-    orgId,
-    clientSecret,
-    privateKey,
-    passphrase = '',
-    metaScopes = [
-      'https://ims-na1.adobelogin.com/s/ent_analytics_bulk_ingest_sdk',
-      'https://ims-na1.adobelogin.com/s/ent_marketing_sdk',
-      'https://ims-na1.adobelogin.com/s/ent_campaign_sdk',
-      'https://ims-na1.adobelogin.com/s/ent_adobeio_sdk',
-      'https://ims-na1.adobelogin.com/s/ent_audiencemanagerplatform_sdk'
-    ],
-    ims = 'https://ims-na1.adobelogin.com'
-  } = options
-
-  const errors = []
-  if (!clientId) { errors.push('clientId') }
-  if (!technicalAccountId) { errors.push('technicalAccountId') }
-  if (!orgId) { errors.push('orgId') }
-  if (!clientSecret) { errors.push('clientSecret') }
-  if (!privateKey) { errors.push('privateKey') }
-  if (!metaScopes || metaScopes.length === 0) { errors.push('metaScopes') }
-  if (!ims) { errors.push('ims') }
-  if (errors.length > 0) {
-    throw new Error(`Required parameter(s) ${errors.join(', ')} are missing`)
-  }
-
-  const jwtPayload = createJwtPayload({ // potentially add the defaults, to options
-    ...options,
-    passphrase,
-    metaScopes,
-    ims
-  })
-
-  const token = jwt.sign(
-    jwtPayload,
-    { key: privateKey, passphrase },
-    { algorithm: 'RS256' }
+  const form = new FormData()
+  Object.entries(postData).forEach(([k, v]) =>
+    form.append(k, v)
   )
 
-  return token
-}
-
-/**
- * Gets an OAuth token by exchanging a JWT.
- *
- * @param {object} options the parameters to send to the jwt exchange endpoint
- * @param {string} options.clientId the jwt client id
- * @param {string} options.clientSecret the jwt client secret
- * @param {string} [options.ims='https://ims-na1.adobelogin.com'] the IMS endpoint
- * @param {string} signedJwt the signed JWT
- * @returns {object} the access token
- */
-async function getJWTToken (options, signedJwt) {
-  const {
-    clientId,
-    clientSecret,
-    ims = 'https://ims-na1.adobelogin.com'
-  } = options
-
-  if (!signedJwt) {
-    signedJwt = await getSignedJwt(options)
+  let res
+  try {
+    res = await fetch(
+      IMS_ENDPOINTS[env] + '/ims/token/v2',
+      {
+        method: 'POST',
+        headers: form.getHeaders(),
+        body: form
+      }
+    )
+  } catch (e) {
+    throw new Error(`cannot send request to IMS: ${e.message}`)
   }
 
-  const form = new FormData()
-  form.append('client_id', clientId)
-  form.append('client_secret', clientSecret)
-  form.append('jwt_token', signedJwt)
-
-  const postOptions = {
-    method: 'POST',
-    body: form,
-    headers: form.getHeaders()
+  if (res.ok) {
+    return res.json()
   }
-
-  const res = await fetch(`${ims}/ims/exchange/jwt/`, postOptions)
-  const json = await res.json()
-  const { access_token, error, error_description } = json
-  if (!access_token) {
-    if (error && error_description) {
-      throw new Error(`${error}: ${error_description}`)
-    } else {
-      throw new Error(`An unknown error occurred while swapping jwt. The response is as follows: ${JSON.stringify(json)}`)
-    }
-  }
-  return json
+  throw new Error(`error response from IMS with status: ${res.status} and body: ${await res.text()}`)
 }
 
 module.exports = {
-  JWT_EXPIRY_SECONDS,
-  createJwtPayload,
-  getSignedJwt,
-  getJWTToken,
-  getOauthToken
+  getAccessTokenByClientCredentials
 }
